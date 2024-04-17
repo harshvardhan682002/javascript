fd1e8f35b4a4d11e8bf1c230872cbc6b25700ce2
harshvardhan682002 <109468215+harshvardhan682002@users.noreply.github.com>
2024-04-17 22:19:37 +0530

    Delete addArr.js

--- addArr.js
+++
@@ -1,9 +0,0 @@
-function newArr(num){
-    console.log(num);
-    let sum = 0;
-    for(i = 0;i< num.length;i++){
-        sum = sum + num[i];
-    }
-console.log('enter the number',sum); 
-}
-newArr([1,2,3,4,5,6,7,8]);
